0b59fe793dc4b101c8780d2f29d633d64da02821
Woodoocubic <guangqingliu1985@gmail.com>
2020-02-26 21:09:19 +0800

    leetCode994
    refer to https://www.cnblogs.com/asenyang/p/10391257.html

---
+++ LeetCode994/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace LeetCode994
+{
+    class Program
+    {
+        public class Solution
+        {
+            public int OrangeRotting(int[][] grid)
+            { 
+                int[] horiDirec = new int[]{ 1, -1, 0, 0};
+                int[] vertDirec = new int[]{ 0, 0, 1, -1};
+                List<int[]> rotList = new List<int[]>();
+                int minute = 0;
+
+                for (int i = 0; i < grid.GetLength(1) ; i++)
+                {
+                    for (int j = 0; j < grid.GetLength( 0); j++)
+                    {
+                        if (grid[i][j] == 2)
+                        {
+                            rotList.Add(new int[] {i,j});
+                        }
+                    }
+                }
+
+                int size = rotList.Count();
+                while (size-- !=0 )
+                {
+                    List<int[]> newRotList = new List<int[]>();
+                    foreach (var node in rotList)
+                    {
+                        int x0 = node[0];
+                        int y0 = node[1];
+                        for (int k = 0; k < 4; k++)
+                        {
+                            int x = x0 + horiDirec[k];
+                            int y = y0 + vertDirec[k];
+
+                            bool xRange = x >= 0 && x < grid.GetLength(0);
+                            bool yRange = y >= 0 && y < grid.GetLength(1);
+                            if (xRange && yRange && grid[x][y]==1)
+                            {
+                                grid[x][y] = 2;
+                                newRotList.Add(new int[]{x,y});
+                            }
+                        }
+                    }
+
+                    if (!newRotList.Any())
+                    {
+                        break;
+                    }
+
+                    minute++;
+                }
+
+                foreach (var item in grid)
+                {
+                    foreach (var num in item)
+                    {
+                        if (num ==1) {return -1;}
+                    }
+                }
+
+                return minute;
+            }
+        }
+    }
+}
---
+++ LeetCode994/RottingOranges.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode994
+{
+    public class RottingOranges
+    {
+        private int row = 0;
+        private int column = 0;
+        private int freshOrangeCount = 0;
+        private int rottenOrangeCount = 0;
+        private int minute = 0;
+        private Queue<int[]> queue = new Queue<int[]>();
+        private int[,] tagGrid;
+
+        public void BFS(int[][] grid)
+        {
+            var rottingList = new List<int[]>();
+
+            while (queue.Count>0)
+            {
+                while (queue.Count>0)
+                {
+                    var orange = queue.Dequeue();
+                    int i = orange[0];
+                    int j = orange[1];
+
+                    int i_new = -1;
+                    int j_new = -1;
+
+                    //up
+                    i_new = i - 1;
+                    j_new = j;
+                    if (i_new >= 0 && tagGrid[i_new, j_new] == 0 && grid[i_new][j_new] == 1)
+                    {
+                        freshOrangeCount--;
+                        rottenOrangeCount++;
+                        tagGrid[i_new, j_new] = 1;
+                        grid[i_new][j_new] = 2;
+                        rottingList.Add(new int[] {i_new, j_new});
+                    }
+
+                    //down
+                    i_new = i + 1;
+                    j_new = j;
+                    if (i_new <= row - 1 && tagGrid[i_new, j_new] == 0 && grid[i_new][j_new] == 1)
+                    {
+                        freshOrangeCount--;
+                        rottenOrangeCount++;
+                        tagGrid[i_new, j_new] = 1;
+                        grid[i_new][j_new] = 2;
+                        rottingList.Add(new int[] {i_new, j_new});
+                    }
+
+                    //left: j-1 >=0
+                    i_new = i;
+                    j_new = j - 1;
+                    if (j_new >= 0 && tagGrid[i_new, j_new] == 0 && grid[i_new][j_new] == 1)
+                    {
+                        freshOrangeCount--;
+                        rottenOrangeCount++;
+                        tagGrid[i_new, j_new] = 1;
+                        grid[i_new][j_new] = 2;
+                        rottingList.Add(new int[] {i_new, j_new});
+                    }
+
+                    //right: j+1 <= column-1;
+                    i_new = i;
+                    j_new = j + 1;
+                    if (j_new <= column - 1 && tagGrid[i_new, j_new] == 0 && grid[i_new][j_new] == 1)
+                    {
+                        freshOrangeCount--;
+                        rottenOrangeCount++;
+                        tagGrid[i_new, j_new] = 1;
+                        grid[i_new][j_new] = 2;
+                        rottingList.Add(new int[] {i_new, j_new});
+                    }
+                }
+
+                if (rottingList.Any())
+                {
+                    minute++;
+                }
+
+                foreach (var item in rottingList)
+                {
+                    queue.Enqueue(item);
+                }
+                rottingList.Clear();
+            }
+        }
+
+        public int OrangesRotting(int[][] grid)
+        {
+            row = grid.Length;
+            column = grid[0].Length;
+            tagGrid = new int[row, column];
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    tagGrid[i, j] = 0;
+                    if (grid[i][j] == 1)
+                    {
+                        freshOrangeCount++;
+                    }
+
+                    if (grid[i][j] == 2)
+                    {
+                        rottenOrangeCount++;
+                        queue.Enqueue(new int[] {i, j});
+                    }
+                }
+            }
+
+            BFS(grid);
+
+            if (freshOrangeCount !=0)
+            {
+                return -1;
+            }
+            else
+            {
+                return minute;
+            }
+        }
+    }
+}
