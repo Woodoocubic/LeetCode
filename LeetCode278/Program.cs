c18a34ba55eafe1d7ff97cdd9a16482eebbfa2f6
Woodoocubic <guangqingliu1985@gmail.com>
2020-03-03 20:36:45 +0800

    update 2 easy

---
+++ LeetCode278/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LeetCode278
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+        }
+
+        public int FirstBadVersion(int n)
+        {
+            int lo = 1;
+            int hi = n;
+            while (lo < hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+                if (IsBadVersion(mid))
+                {
+                    hi = mid;
+                }
+                else
+                {
+                    lo = mid + 1;
+                }
+            }
+
+            return hi;
+        }
+    }
+}
